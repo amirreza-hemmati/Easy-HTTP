e2bd67e4fa11d5ee9aafe93be7bcb1f62e60f118
amirreza-hemmati <48690031+amirreza-hemmati@users.noreply.github.com>
2019-09-19 00:28:20 +0430

    finish easy http

--- app.js
+++ app.js
@@ -11,31 +11,37 @@ const aSet = alink[1];
 const aPut = alink[2];
 const aDelete = alink[3];
 const forms = document.forms[0];
+const inputNumber = forms[0];
+const inputTitle = forms[1];
+const inputBody = forms[2];
+let sum = 0;
 
-  // function
+// function
 
-    // GET Data
+// GET Data
 aGet.addEventListener("click", e => {
   e.preventDefault();
   myform.showform();
-  myform.hideInput(true,false,false);
+  myform.hideInput(true, false, false);
   forms.onsubmit = e => {
     e.preventDefault();
-    myform.submits(true,false,false,getData);
-  }
-  aGet.style.display = "none";
+    myform.submits(true, false, false, getData);
+  };
+  e.target.style.display = "none";
   function getData() {
     const vEasyhttp = new easyHttp();
-    vEasyhttp.getPosts("https://jsonplaceholder.typicode.com/posts",
-      posts => {
-        const posts1 = JSON.parse(posts);
-        cardGet.innerHTML = "";
-        for(let index = 0 ; index < posts1.length ; index++){
-          if(index >= eval(forms[0].value)) {
-            break;
-          }
-          let el = posts1[index];
-          cardGet.innerHTML += `
+    vEasyhttp.getPosts(
+      "https://jsonplaceholder.typicode.com/posts",
+      (err, posts) => {
+        if (!err) {
+          const posts1 = JSON.parse(posts);
+          cardGet.innerHTML = "";
+          for (let index = 0; index < posts1.length; index++) {
+            if (index >= eval(forms[0].value)) {
+              break;
+            }
+            let el = posts1[index];
+            cardGet.innerHTML += `
             <br />
               <span class="text-danger">ID:</span> ${index + 1}
               <br /><br />
@@ -46,11 +52,99 @@ aGet.addEventListener("click", e => {
               <br>
             <hr>
           `;
+          }
+          myform.finishWork();
+          aGet.style.display = "block";
+        } else {
+          console.log(err);
         }
-        myform.finishWork();
-        aGet.style.display = "block";
       }
     );
   }
 });
 
+// SET Data
+aSet.addEventListener("click", e => {
+  e.preventDefault();
+  myform.showform();
+  myform.hideInput(false, true, true);
+  forms.onsubmit = e => {
+    e.preventDefault();
+    myform.submits(false, true, true, putdata);
+  };
+  e.target.style.display = "none";
+
+  function putdata() {
+    const httpSet = new easyHttp();
+    const mydata = {title: inputTitle.value, body: inputBody.value };
+    httpSet.setPosts(
+      "https://jsonplaceholder.typicode.com/posts",
+      mydata,
+      getpost => {
+        let arrGetPost = JSON.parse(getpost)
+        cardSet.innerHTML = `
+          <br />
+          <span class="text-danger">ID:</span> ${arrGetPost.id + sum}
+          <br /><br />
+          <span class="text-danger">Title:</span> ${arrGetPost.title}
+          <br /><br />
+          <span class="text-danger">Body:</span> ${arrGetPost.body}
+        `
+        sum += 1;
+      }
+      );
+    myform.finishWork();
+    aSet.style.display = "block";
+  }
+});
+
+// PUT Data
+aPut.addEventListener("click", e => {
+  e.preventDefault();
+  myform.showform();
+  myform.hideInput(true, true, true);
+  forms.onsubmit = e => {
+    e.preventDefault();
+    myform.submits(true, true, true, putdata);
+  };
+  e.target.style.display = "none";
+
+  function putdata() {
+    const easyPost = new easyHttp();
+    const putDatas = {title: inputTitle.value , body: inputBody.value};
+    easyPost.putData(`https://jsonplaceholder.typicode.com/posts/${inputNumber.value}` , putDatas , posts => {
+      let arrGetPost = JSON.parse(posts);
+      cardPut.innerHTML = `
+        <br />
+        <span class="text-danger">ID:</span> ${arrGetPost.id + sum}
+        <br /><br />
+        <span class="text-danger">Title:</span> ${arrGetPost.title}
+        <br /><br />
+        <span class="text-danger">Body:</span> ${arrGetPost.body}
+      `
+    });
+    myform.finishWork();
+    aPut.style.display = "block";
+  }
+});
+
+// DELETE Data
+aDelete.addEventListener('click' , e => {
+  e.preventDefault();
+  myform.showform();
+  myform.hideInput(true, false, false);
+  forms.onsubmit = e => {
+    e.preventDefault();
+    myform.submits(true, false, false, putdata);
+  };
+  e.target.style.display = "none";
+
+  function putdata(){
+    const easyDelete = new easyHttp();
+    easyDelete.deleteData(`https://jsonplaceholder.typicode.com/posts/${inputNumber.value}` , e => {
+      cardDelete.innerHTML = `<p class="text-center text-danger">Deleted Data</p>`;
+    });
+    myform.finishWork();
+    aDelete.style.display = "block";
+  }
+})
--- easyHTTP.js
+++ easyHTTP.js
@@ -8,14 +8,50 @@ class easyHttp{
         this.http.open("GET" , url , true);
         this.http.onload = e => {
             if(this.http.status === 200 && this.http.readyState === 4){
-                callback(this.http.responseText);
+                callback(null,this.http.responseText);
+            }
+            else{
+                callback(`Error: ${this.http.status}`);
             }
         }
         this.http.send();
     }
 
     // Make an HTTP request by SET
-    setPosts() {
-
+    setPosts(url , data , callback) {
+        this.http.open("POST" , url , true);
+        this.http.setRequestHeader('Content-type' , 'application/json');
+        this.http.onload = () => {
+            if(this.http.status == 201 || this.http == 200 && this.http.readyState === 4){
+                callback(this.http.responseText);
+            }
+            else{
+                alert(`Error: ${this.http.status}`);
+            }
+        };
+        this.http.send(JSON.stringify(data));
     }
-}
+
+    // Make an HTTP request by PUT
+    putData(url , data , callback){
+        this.http.open("PUT" , url , true);
+        this.http.setRequestHeader('Content-type' , 'application/json')
+        this.http.onload = e => {
+            callback(this.http.responseText);
+        }
+        this.http.send(JSON.stringify(data));
+    }
+
+    // Make an HTTP request by PUT
+    deleteData(url , callback){
+        this.http.open("DELETE" , url , true);
+        this.http.onload = e => {
+            if(this.http.status === 200){
+                callback();
+            }else{
+                console.log(`Error: ${this.http.status}`)
+            }
+        }
+        this.http.send();
+    }
+};
